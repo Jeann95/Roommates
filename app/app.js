8147396967ca533e5ff1cfcc539dbb040a827cdc
Stef Mikloska <smikloska@gmail.com>
2014-10-04 15:35:54 -0400

    fixes for restructure

--- app/app.js
+++ app/app.js
@@ -8,26 +8,11 @@
  *
  * Main module of the application.
  */
-angular
-  .module('roommatesApp', [
+angular.module('roommatesApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+]);
