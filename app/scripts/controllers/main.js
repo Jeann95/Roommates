b0b11b3f192dd981800b4ef8b898424189e5f3ce
Stef Mikloska <smikloska@gmail.com>
2014-10-04 13:17:36 -0400

    base scaffolding with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name roommatesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the roommatesApp
+ */
+angular.module('roommatesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
